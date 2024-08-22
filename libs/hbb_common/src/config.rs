a811ef2d1b3fd34d3c28aa13ffd133c27a2e1801
wecker88 <140624077+wecker88@users.noreply.github.com>
2024-08-22 23:29:26 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["aws.118558.xyz"];
+pub const PUBLIC_RS_PUB_KEY: &str = "vMMEw6xyr+RtFwgewWLbNSB+g7Y2jPqhrJdSRuheplk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
